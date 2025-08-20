4ca0842dccc9d324b99910f5db3ee5ded880bf90
Oatkrub <sirawit.vir@builk.com>
2025-08-20 14:24:06 +0700

    add basic routing and database pool connection

--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	config_env "omnicam.com/backend/config"
 	api_routes "omnicam.com/backend/internal/routes"
+	db_client "omnicam.com/backend/pkg/db"
 	"omnicam.com/backend/pkg/logger"
 )
 
 func main() {
+
 	logger := logger.InitLogger()
 	defer logger.Sync()
 	env := config_env.InitAppEnv(logger)
-	router := api_routes.InitRoutes(logger, env)
+	client_db := db_client.InitDatabase(env)
+	router := api_routes.InitRoutes(logger, env, client_db.Queries)
 	router.Run()
 }
