4e59f361d6c3f2d1b8a24bf7c460829cf7fd5f4e
Oatkrub <sirawit.vir@builk.com>
2025-08-20 14:24:06 +0700

    add basic routing and database pool connection

--- backend/pkg/db/client.go
+++ backend/pkg/db/client.go
@@ -1,14 +1,39 @@
 package db_client
 
-// func Migrate() {
-// 	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
-// 	m, err := migrate.NewWithDatabaseInstance(
-// 		"file:///migrations",
-// 		"postgres", driver)
+import (
+	"context"
+	"fmt"
+	"os"
 
-// 	driver, err := postgres.WithInstance(pool, &postgres.Config{})
-// 	m, err := migrate.NewWithDatabaseInstance(
-// 		"file:///migrations",
-// 		"postgres", driver)
-// 	m.Up() // or m.Steps(2) if you want to explicitly set the number of migrations to run
-// }
+	"github.com/jackc/pgx/v5/pgxpool"
+	// "go.uber.org/zap/internal/pool"
+	config_env "omnicam.com/backend/config"
+	db_sqlc_gen "omnicam.com/backend/pkg/db/sqlc-gen"
+)
+
+type DB struct {
+	Queries *db_sqlc_gen.Queries
+	Pool    *pgxpool.Pool
+}
+
+func InitDatabase(env *config_env.AppEnv) *DB {
+	pool, err := pgxpool.New(context.Background(), env.DatabaseUrl)
+	if err != nil {
+		fmt.Println("Failed to create DB pool:", err)
+		os.Exit(1)
+	}
+
+	err = pool.Ping(context.Background())
+
+	if err != nil {
+		fmt.Println("Failed to create DB pool:", err)
+		os.Exit(1)
+	}
+
+	query := db_sqlc_gen.New(pool)
+
+	return &DB{
+		Queries: query,
+		Pool:    pool,
+	}
+}
