9b2ef6307627fddc24c038543b16793216b54dc8
NewBieCoderXD <fruity5555@gmail.com>
2025-08-21 13:29:39 +0700

    add swagger command, add readme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   ssr: true,
   devtools: { enabled: true },
   srcDir: "frontend",
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/eslint",
-    ...(import.meta.dev ? ["@scalar/nuxt"] : []),
-  ],
-  ...(import.meta.dev && {
-    scalar: {
-      pathRouting: {
-        basePath: "/docs",
-      },
-      url: "/openapi.yaml",
-    },
-  }),
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
   app: {
     head: {
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
