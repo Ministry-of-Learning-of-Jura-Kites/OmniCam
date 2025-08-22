4b3942a492a0b13ac58409a566720c77b1584703
NewBieCoderXD <fruity5555@gmail.com>
2025-08-22 19:44:50 +0700

    refactor to router group

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	config_env "omnicam.com/backend/config"
 	api_routes "omnicam.com/backend/internal/routes"
 	db_client "omnicam.com/backend/pkg/db"
@@ -8,11 +9,22 @@ import (
 )
 
 func main() {
-
 	logger := logger.InitLogger()
 	defer logger.Sync()
+
 	env := config_env.InitAppEnv(logger)
+
 	client_db := db_client.InitDatabase(env)
-	router := api_routes.InitRoutes(logger, env, client_db.Queries)
+
+	router := gin.Default()
+
+	var apiV1 gin.IRouter
+	apiV1 = router.Group("/api/v1")
+	api_routes.InitRoutes(api_routes.Dependencies{
+		Logger: logger,
+		Env:    env,
+		DB:     client_db.Queries,
+	}, apiV1)
+
 	router.Run()
 }
