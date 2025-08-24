6c6052016d223e17a3adc4f2e389ec12be7c893d
NewBieCoderXD <fruity5555@gmail.com>
2025-08-24 19:19:15 +0700

    fix tailwind with shadcn

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import { Button } from "@/components/ui/button";
 const { data } = await useFetch("/api/test");
 </script>
 <template>
-  <span class="bg-green-100">test {{ data }}</span>
+  <div>
+    <span class="bg-green-100">test {{ data }}</span>
+    <Button>Button</Button>
+  </div>
 </template>
