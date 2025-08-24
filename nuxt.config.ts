1aba80147adcfd9042fad2597c0d4e5d114daca4
Oatkrub <sirawit.vir@builk.com>
2025-08-24 14:12:18 +0700

    config shadcn nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   compatibilityDate: "2025-07-15",
   ssr: true,
+  // css: ["~/assets/css/tailwind.css"],
   devtools: { enabled: true },
   srcDir: "frontend",
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "shadcn-nuxt"],
   app: {
     head: {
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
@@ -17,5 +22,16 @@ export default defineNuxtConfig({
   serverDir: "frontend/server",
   nitro: {
     plugins: ["plugins/runtime-env.server.ts"],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
   },
 });
