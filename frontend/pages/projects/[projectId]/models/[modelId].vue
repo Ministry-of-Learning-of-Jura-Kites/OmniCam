a981eb4ff70bcbf3aac3b1b2a80f0477b18f0974
SigmaFong <ahfong2548@gmail.com>
2025-08-24 15:16:18 +0700

    feat: added projects and model pages with appropriate layouts and components.

---
+++ frontend/pages/projects/[projectId]/models/[modelId].vue
@@ -0,0 +1,14 @@
+<template>
+  <LayoutModelsPage>
+    <Scene3D :modelId="modelId" />
+  </LayoutModelsPage>
+</template>
+
+<script setup>
+import LayoutModelsPage from "@/layouts/modelsPage.vue";
+import Scene3D from "@/components/Scene3D.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const modelId = route.params.modelId;
+</script>
