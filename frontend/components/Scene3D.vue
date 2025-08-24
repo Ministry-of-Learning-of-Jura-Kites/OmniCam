a56463e0d8a085a9138e3751d9c3bd4c2bb5746b
NewBieCoderXD <fruity5555@gmail.com>
2025-08-24 22:09:07 +0700

    Add Yaw, Pitch camera logic

--- frontend/components/Scene3D.vue
+++ frontend/components/Scene3D.vue
@@ -1,11 +1,66 @@
+<script setup lang="ts">
+import { TresCanvas } from "@tresjs/core";
+import { Grid, Environment } from "@tresjs/cientos";
+import type { PerspectiveCamera } from "three";
+import { Euler } from "three";
+
+defineProps<{
+  modelId?: string | null;
+  placeholderText?: string | null;
+}>();
+
+const camera: Ref<PerspectiveCamera | null> = ref(null);
+
+let lastX = 0;
+let lastY = 0;
+const isDragging = ref(false);
+
+function onPointerDown(e: PointerEvent) {
+  isDragging.value = true;
+  lastX = e.clientX;
+  lastY = e.clientY;
+}
+
+function onPointerUp(_e: PointerEvent) {
+  isDragging.value = false;
+}
+
+const maxPitch = Math.PI / 2 - 0.01;
+const minPitch = -Math.PI / 2 + 0.01;
+
+let yaw = 0;
+let pitch = 0;
+
+function onPointerMove(e: PointerEvent) {
+  if (!isDragging.value) return;
+  const deltaX = e.clientX - lastX;
+  yaw -= deltaX * 0.01;
+  camera.value!.rotation.y = yaw;
+
+  const deltaY = e.clientY - lastY;
+  pitch -= deltaY * 0.01;
+  pitch = Math.max(minPitch, Math.min(maxPitch, pitch));
+  camera.value!.rotation.x = pitch;
+
+  lastX = e.clientX;
+  lastY = e.clientY;
+}
+</script>
+
 <template>
   <ClientOnly>
     <div class="w-full h-full bg-background">
-      <TresCanvas clear-color="#0E0C29">
+      <TresCanvas
+        clear-color="#0E0C29"
+        @pointerup="onPointerUp"
+        @pointerdown="onPointerDown"
+        @pointermove="onPointerMove"
+      >
         <!-- Camera -->
         <TresPerspectiveCamera
+          ref="camera"
           :position="[4, 4, 4]"
-          :look-at="[0, 1, 0]"
+          :rotation="new Euler(0, 0, 0, 'YXZ')"
           :fov="75"
         />
 
@@ -39,24 +94,14 @@
         />
 
         <!-- Camera controls from tresjs/cientos -->
-        <OrbitControls
+        <!-- <OrbitControls
           :enable-pan="true"
           :enable-zoom="true"
           :enable-rotate="true"
           :min-distance="2"
           :max-distance="50"
-        />
+        /> -->
       </TresCanvas>
     </div>
   </ClientOnly>
 </template>
-
-<script setup lang="ts">
-import { TresCanvas } from "@tresjs/core";
-import { OrbitControls, Grid, Environment } from "@tresjs/cientos";
-
-defineProps<{
-  modelId?: string | null;
-  placeholderText?: string | null;
-}>();
-</script>
