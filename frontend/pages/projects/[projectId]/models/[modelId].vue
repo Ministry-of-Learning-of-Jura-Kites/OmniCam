d54a3ea1f0cbc1399194c2dea24894fd998ed1c3
NewBieCoderXD <fruity5555@gmail.com>
2025-08-25 11:43:04 +0700

    enable spectator movement

--- frontend/pages/projects/[projectId]/models/[modelId].vue
+++ frontend/pages/projects/[projectId]/models/[modelId].vue
@@ -1,6 +1,6 @@
 <script setup>
 import LayoutModelsPage from "@/layouts/modelsPage.vue";
-import Scene3D from "@/components/Scene3D.vue";
+import Scene3D from "~/components/scene-3d/Scene3D.vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
