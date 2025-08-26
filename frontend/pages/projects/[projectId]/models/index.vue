cf5035c54a0994ed303a5cc811c12587f5211e89
SigmaFong <ahfong2548@gmail.com>
2025-08-26 13:45:57 +0700

    feat: add simple CameraPanel component into modelsPage layout

--- frontend/pages/projects/[projectId]/models/index.vue
+++ frontend/pages/projects/[projectId]/models/index.vue
@@ -1,10 +1,10 @@
+<script setup>
+import LayoutModelsPage from "@/layouts/modelsPage.vue";
+import Scene3D from "@/components/Scene3D.vue";
+</script>
+
 <template>
   <LayoutModelsPage>
     <Scene3D :modelId="null" placeholderText="Select a model to display" />
   </LayoutModelsPage>
 </template>
-
-<script setup>
-import LayoutModelsPage from "@/layouts/modelsPage.vue";
-import Scene3D from "@/components/Scene3D.vue";
-</script>
