3505a019b149747e68d6b90e52e360024eb2cc1d
Oatkrub <sirawit.vir@builk.com>
2025-08-27 16:21:52 +0700

    fix lint

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,5 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts">
-import { Button } from "@/components/ui/button";
-const { data } = await useFetch("/api/test");
-</script>
+<script setup lang="ts"></script>
 <template>
   <header>
     show all project / include create clone and routing to project id
