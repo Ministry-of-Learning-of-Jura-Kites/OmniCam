02ac0cce966d2afa0d7edff69b3c2c93de0b8d70
Oatkrub <sirawit.vir@builk.com>
2025-08-28 16:07:31 +0700

    initialize page / code pattern

--- frontend/pages/projects/[projectId]/index.vue
+++ frontend/pages/projects/[projectId]/index.vue
@@ -1,7 +1,56 @@
-<script lang="ts"></script>
+<script setup lang="ts">
+export interface Model {
+  id: string;
+  projectId: string;
+  name: string;
+  description: string;
+  version: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ModelGetRequest {
+  data: Model[];
+}
+
+export interface ModelCreateRequest {
+  name: string;
+  description: string;
+}
+
+export interface ModelUpdateRequest {
+  name: string;
+  description: string;
+}
+const config = useRuntimeConfig();
+const route = useRoute();
+
+const models = ref<Model[]>();
+
+async function fetchModel() {
+  const projectId = route.params.projectId as string;
+  console.log(projectId, "project id");
+
+  const response = await $fetch<ModelGetRequest>(
+    `${config.public.NUXT_PUBLIC_URL}/api/v1/projects/${projectId}/models`,
+    {
+      method: "GET",
+    },
+  );
+
+  models.value = response.data;
+}
+
+onMounted(() => {
+  fetchModel();
+});
+</script>
 <template>
-  <header>
+  <div v-for:="model in models">
+    <h1>{{ model.name }}</h1>
+  </div>
+  <!-- <header>
     list all models including create model / update model (not camera btw)
     pathing to /models/:modelId
-  </header>
+  </header> -->
 </template>
