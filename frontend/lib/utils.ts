3a7ca4b518fa5b7883e19edfdf8d1b817b66f869
Oatkrub <sirawit.vir@builk.com>
2025-09-25 12:29:52 +0700

    ui / pagination / logic on create and delete / customize ui

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,16 +1,7 @@
-import type { ClassValue } from "clsx";
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-import * as THREE from "three";
+import type { ClassValue } from "clsx"
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function getAxisVector(axis: "x" | "y" | "z") {
-  return new THREE.Vector3(
-    axis === "x" ? 1 : 0,
-    axis === "y" ? 1 : 0,
-    axis === "z" ? 1 : 0,
-  );
+  return twMerge(clsx(inputs))
 }
