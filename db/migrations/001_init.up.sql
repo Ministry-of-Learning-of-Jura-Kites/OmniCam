24157a5b700d24cd742c4ba2ba87097c15d8c5e5
NewBieCoderXD <fruity5555@gmail.com>
2025-09-26 15:23:53 +0700

    fix migrations down

--- db/migrations/001_init.up.sql
+++ db/migrations/001_init.up.sql
@@ -63,23 +63,3 @@ CREATE TABLE "user_model_workspace" (
   created_at TIMESTAMPTZ DEFAULT NOW(),
   updated_at TIMESTAMPTZ DEFAULT NOW()
 );
-
--- CREATE TABLE "camera" (
---   id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
---   name VARCHAR(255) NOT NULL,
---   angle_x DOUBLE PRECISION,
---   angle_y DOUBLE PRECISION,
---   angle_z DOUBLE PRECISION,
---   angle_w DOUBLE PRECISION,
---   pos_x DOUBLE PRECISION,
---   pos_y DOUBLE PRECISION,
---   pos_z DOUBLE PRECISION,
---   -- always belongs to a model
---   model_id UUID NOT NULL REFERENCES "model" (id) ON DELETE CASCADE,
---   -- optionally belongs to a user (snapshot case)
---   user_id UUID REFERENCES "user" (id) ON DELETE CASCADE,
---   -- auto derived: true if snapshot, false if model camera
---   is_snapshot BOOLEAN generated always AS (user_id IS NOT NULL) stored,
---   -- if user_id is not null → must exist in user_model_snapshots
---   CONSTRAINT fk_camera_snapshot FOREIGN key (model_id, user_id) REFERENCES "user_model_snapshots" (model_id, user_id) ON DELETE CASCADE
--- );
