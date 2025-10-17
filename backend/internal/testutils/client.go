a566c815c9152488e8d8a6ef534978a01ff3310e
NewBieCoderXD <fruity5555@gmail.com>
2025-10-17 21:01:05 +0700

    feat: enhance workspace creation and retrieval tests with improved error handling and data seeding

--- backend/internal/testutils/client.go
+++ backend/internal/testutils/client.go
@@ -23,13 +23,15 @@ import (
 	db_sqlc_gen "omnicam.com/backend/pkg/db/sqlc-gen"
 )
 
-var TestDb *TestDbStruct
-
-var DbSem = make(chan struct{}, 5)
-
-var LockTemplateDb sync.Mutex
-
-var initOnce sync.Once
+var (
+	TestDb         *TestDbStruct
+	DbNames        = make(chan string, maxPoolSize)
+	DbPools        = map[string]*pgxpool.Pool{}
+	LockTemplateDb sync.Mutex
+	initOnce       sync.Once
+	currentCount   = 0
+	maxPoolSize    = 5
+)
 
 type TestDbStruct struct {
 	DSN      string
@@ -149,7 +151,7 @@ func startTestDB(ctx context.Context) (*TestDbStruct, error) {
 	}, nil
 }
 
-func InitTestDbIfNotAlready() {
+func GetTestDb(ctx context.Context) (string, *pgxpool.Pool, error, func()) {
 	initOnce.Do(func() {
 		ctx := context.Background()
 
@@ -160,79 +162,157 @@ func InitTestDbIfNotAlready() {
 
 		TestDb = testDb
 	})
+
+	// Allocate 1 db
+	var dbName string
+	select {
+	case dbName = <-DbNames:
+	default:
+		LockTemplateDb.Lock()
+		if currentCount < maxPoolSize {
+			id, _ := uuid.NewUUID()
+			dbName := fmt.Sprintf("omnicam_%s", id.String())
+
+			adminConn, err := pgx.Connect(ctx, TestDb.DSN)
+			if err != nil {
+				LockTemplateDb.Unlock()
+				return "", nil, fmt.Errorf("connect admin: %w", err), nil
+			}
+			_, err = adminConn.Exec(ctx, fmt.Sprintf(`CREATE DATABASE "%s" WITH TEMPLATE omnicam OWNER postgres;`, dbName))
+			adminConn.Close(ctx)
+			if err != nil {
+				LockTemplateDb.Unlock()
+				return "", nil, fmt.Errorf("create database: %w", err), nil
+			}
+
+			currentCount++
+			LockTemplateDb.Unlock()
+
+			cfg, err := pgxpool.ParseConfig(TestDb.DSN)
+			if err != nil {
+				return "", nil, err, nil
+			}
+			cfg.ConnConfig.Database = dbName
+
+			conn, err := pgxpool.NewWithConfig(ctx, cfg)
+			if err != nil {
+				return "", nil, err, nil
+			}
+			DbPools[dbName] = conn
+			return dbName, conn, nil, func() {
+				DbNames <- dbName
+			}
+		} else {
+			// Wait for an available DB from the pool
+			LockTemplateDb.Unlock()
+			dbName = <-DbNames
+		}
+	}
+
+	return dbName, DbPools[dbName], nil, func() {
+		DbNames <- dbName
+	}
+
+	// DbSem <- struct{}{}
+
+	// id, err := uuid.NewUUID()
+	// if err != nil {
+	// 	return nil, err, nil
+	// }
+
+	// newDbName := fmt.Sprintf("omnicam_%s", id.String())
+
+	// semReleased := false
+	// defer func() {
+	// 	if !semReleased {
+	// 		<-DbSem
+	// 	}
+	// }()
+
+	// // Create database
+	// err = func() error {
+	// 	LockTemplateDb.Lock()
+	// 	defer func() {
+	// 		LockTemplateDb.Unlock()
+	// 	}()
+	// 	log.Printf("Creating DB %s: Acquired a lock", newDbName)
+
+	// 	adminConn, err := pgx.Connect(ctx, TestDb.DSN)
+	// 	if err != nil {
+	// 		return fmt.Errorf("connect admin: %w", err)
+	// 	}
+	// 	defer adminConn.Close(ctx)
+
+	// 	_, err = adminConn.Exec(ctx, fmt.Sprintf(`CREATE DATABASE "%s" WITH TEMPLATE omnicam OWNER postgres;`, newDbName))
+	// 	if err != nil {
+	// 		return fmt.Errorf("create database: %w", err)
+	// 	}
+
+	// 	return nil
+	// }()
+	// if err != nil {
+	// 	return nil, err, nil
+	// }
+
+	// log.Printf("Created DB %s: Release a lock", newDbName)
+
+	// // Connect to new database
+	// cfg, err := pgxpool.ParseConfig(TestDb.DSN)
+	// if err != nil {
+	// 	return nil, err, nil
+	// }
+
+	// cfg.ConnConfig.Database = newDbName
+
+	// conn, err := pgxpool.NewWithConfig(ctx, cfg)
+	// if err != nil {
+	// 	return nil, err, nil
+	// }
+
+	// semReleased = true
+	// return conn, nil, func() {
+	// 	adminConn, err := pgx.Connect(ctx, TestDb.DSN)
+	// 	if err != nil {
+	// 		log.Fatalf("connect admin: %v", err)
+	// 	}
+	// 	defer adminConn.Close(ctx)
+
+	// 	_, err = adminConn.Exec(ctx, fmt.Sprintf(`DROP DATABASE "%s";`, newDbName))
+	// 	if err != nil {
+	// 		log.Fatalf("error while dropping database %s: %v", newDbName, err)
+	// 	}
+	// 	log.Printf("Destroyed DB %s: releasing semaphore...", newDbName)
+	// 	<-DbSem
+	// }
 }
 
-func GetTestDb(ctx context.Context) (*pgxpool.Pool, error, func()) {
-	// Allocate 1 db
-	DbSem <- struct{}{}
+func Truncate(ctx context.Context, dbName string) error {
+	log.Printf("Truncating %s", dbName)
 
-	id, err := uuid.NewUUID()
+	conn := DbPools[dbName]
+	rows, err := conn.Query(ctx, "SELECT tablename FROM pg_tables WHERE schemaname = 'public'")
 	if err != nil {
-		return nil, err, nil
+		return err
+	}
+	defer rows.Close()
+
+	var tables []string
+	for rows.Next() {
+		var tableName string
+		if err := rows.Scan(&tableName); err != nil {
+			return err
+		}
+		tables = append(tables, tableName)
 	}
 
-	newDbName := fmt.Sprintf("omnicam_%s", id.String())
-
-	semReleased := false
-	defer func() {
-		if !semReleased {
-			<-DbSem
-		}
-	}()
-
-	// Create database
-	err = func() error {
-		LockTemplateDb.Lock()
-		defer func() {
-			LockTemplateDb.Unlock()
-		}()
-		log.Printf("Creating DB %s: Acquired a lock", newDbName)
-
-		adminConn, err := pgx.Connect(ctx, TestDb.DSN)
+	// Truncate each table
+	for _, table := range tables {
+		_, err := conn.Exec(ctx, fmt.Sprintf(`TRUNCATE TABLE "%s" RESTART IDENTITY CASCADE`, table)) // RESTART IDENTITY for auto-increment reset
 		if err != nil {
-			return fmt.Errorf("connect admin: %w", err)
+			log.Printf("Error truncating table %s: %v", table, err)
+		} else {
+			log.Printf("Table %s truncated.\n", table)
 		}
-		defer adminConn.Close(ctx)
-
-		_, err = adminConn.Exec(ctx, fmt.Sprintf(`CREATE DATABASE "%s" WITH TEMPLATE omnicam OWNER postgres;`, newDbName))
-		if err != nil {
-			return fmt.Errorf("create database: %w", err)
-		}
-
-		return nil
-	}()
-	if err != nil {
-		return nil, err, nil
 	}
-
-	log.Printf("Created DB %s: Release a lock", newDbName)
-
-	// Connect to new database
-	cfg, err := pgxpool.ParseConfig(TestDb.DSN)
-	if err != nil {
-		return nil, err, nil
-	}
-
-	cfg.ConnConfig.Database = newDbName
-
-	conn, err := pgxpool.NewWithConfig(ctx, cfg)
-	if err != nil {
-		return nil, err, nil
-	}
-
-	semReleased = true
-	return conn, nil, func() {
-		adminConn, err := pgx.Connect(ctx, TestDb.DSN)
-		if err != nil {
-			log.Fatalf("connect admin: %v", err)
-		}
-		defer adminConn.Close(ctx)
-
-		_, err = adminConn.Exec(ctx, fmt.Sprintf(`DROP DATABASE "%s";`, newDbName))
-		if err != nil {
-			log.Fatalf("error while dropping database %s: %v", newDbName, err)
-		}
-		log.Printf("Destroyed DB %s: releasing semaphore...", newDbName)
-		<-DbSem
-	}
+	return nil
 }
