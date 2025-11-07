3eb4dd6722a8b7f8fd23053acdb0cdd3e74cbf0a
NewBieCoderXD <fruity5555@gmail.com>
2025-11-07 09:01:38 +0700

    feat: enhance websocket event handling and protobuf definitions for camera autosave

--- db/queries/workspace/003_put_workspace_cams.sql
+++ db/queries/workspace/003_put_workspace_cams.sql
@@ -1,4 +1,4 @@
--- name: UpdateWorkspaceCams :exec
+-- name: UpdateWorkspaceCams :one
 UPDATE "user_model_workspace"
 SET
   cameras = CASE
@@ -14,4 +14,6 @@ SET
   updated_at = NOW()
 WHERE
   user_id = SQLC.ARG(user_id)::UUID
-  AND model_id = SQLC.ARG(model_id)::UUID;
+  AND model_id = SQLC.ARG(model_id)::UUID
+RETURNING
+  version;
--- frontend/components/3d/scene-3d/use-autosave.ts
+++ frontend/components/3d/scene-3d/use-autosave.ts
@@ -3,7 +3,7 @@ import type {
   CameraSaveEvent,
 } from "~/messages/protobufs/autosave_event";
 import {
-  CameraEventType,
+  CameraAutosaveResponse,
   CameraSaveEventSeries,
 } from "~/messages/protobufs/autosave_event";
 import type { ICamera } from "~/types/camera";
@@ -64,6 +64,17 @@ export function useAutosave(
     }),
   );
 
+  watch(
+    () => sceneStates.websocket?.data.value,
+    async (messageBlob) => {
+      if (messageBlob) {
+        const messageArrayBuf = await (messageBlob as Blob).arrayBuffer();
+        const messageByteArr = new Uint8Array(messageArrayBuf);
+        const resp = CameraAutosaveResponse.decode(messageByteArr);
+      }
+    },
+  );
+
   setInterval(() => {
     if (sceneStates.markedForCheck.size == 0) {
       return;
@@ -83,8 +94,9 @@ export function useAutosave(
       if (cam == undefined) {
         lastSynced.delete(camId);
         changed.push({
-          type: CameraEventType.CAMERA_EVENT_TYPE_DELETE,
-          deleteId: camId,
+          delete: {
+            id: camId,
+          },
         });
         continue;
       }
@@ -92,8 +104,9 @@ export function useAutosave(
       // If is newly added, or changed
       if (prev == undefined || !isEqual(prev, formattedCam)) {
         changed.push({
-          type: CameraEventType.CAMERA_EVENT_TYPE_UPSERT,
-          upsert: formattedCam,
+          upsert: {
+            camera: formattedCam,
+          },
         });
         lastSynced.set(camId, transformCameraToProtoEventWithId(camId, cam));
       }
