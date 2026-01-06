5b4107920d5559e7497d9aeaaf35935bd6774c18
Oatkrub <sirawit.vir@builk.com>
2026-01-06 20:10:37 +0700

    change upload file directory

--- backend/internal/controllers/files/get_file.go
+++ backend/internal/controllers/files/get_file.go
@@ -29,13 +29,13 @@ func (t *FileRoute) userHasProjectAccess(c *gin.Context, userId uuid.UUID, proje
 		PageSize:   1000, // or any large number
 		PageOffset: 0,
 	})
-	fmt.Println(len(projects))
+
 	if err != nil {
 		return false, err
 	}
 
 	for _, p := range projects {
-		fmt.Println("project : ", p)
+
 		if p.ID == projectId {
 			return true, nil
 		}
@@ -46,13 +46,12 @@ func (t *FileRoute) userHasProjectAccess(c *gin.Context, userId uuid.UUID, proje
 // Generic file serving
 func (t *FileRoute) serveFile(c *gin.Context, pathSegments ...string) {
 	filePath := filepath.Join(pathSegments...)
-	fmt.Println("HELLLOOLOLOLs")
-	fmt.Println(filePath)
+
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		c.JSON(http.StatusNotFound, gin.H{"message": "file not found"})
 		return
 	}
-	fmt.Println("exist")
+
 	ext := filepath.Ext(filePath)
 	mimeType := mime.TypeByExtension(ext)
 	if mimeType == "" {
@@ -72,14 +71,13 @@ func (t *FileRoute) getProjectFile(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid projectId"})
 		return
 	}
-	fmt.Println("2")
+
 	userId, err := utils.GetUuidFromCtx(c, "userId")
-	fmt.Println("user id : ", userId)
 	if err != nil {
 		c.JSON(http.StatusForbidden, gin.H{"message": "permission denied"})
 		return
 	}
-	fmt.Println("3")
+
 	hasAccess, err := t.userHasProjectAccess(c, userId, projectId)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "failed to validate access"})
@@ -91,7 +89,7 @@ func (t *FileRoute) getProjectFile(c *gin.Context) {
 		return
 	}
 	fileType := "images"
-	fmt.Println("4")
+
 	// Construct file root/uplodas/images/projectId.ext
 	filePath := fmt.Sprintf(internal.Root+"/uploads/%s/%s.%s", fileType, projectId.String(), fileExt)
 	t.serveFile(c, filePath)
@@ -102,19 +100,19 @@ func (t *FileRoute) getModelFile(c *gin.Context) {
 	projectIdStr := c.Param("projectId")
 	modelIdStr := c.Param("modelId")
 	fileExt := c.Param("fileExt")
-	fmt.Println("is in??")
+
 	projectId, err := uuid.Parse(projectIdStr)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid projectId"})
 		return
 	}
-	fmt.Println("1")
+
 	modelId, err := uuid.Parse(modelIdStr)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "invalid modelId"})
 		return
 	}
-	fmt.Println("2")
+
 	userId, err := utils.GetUuidFromCtx(c, "userId")
 	if err != nil {
 		c.JSON(http.StatusForbidden, gin.H{"message": "permission denied"})
@@ -131,7 +129,7 @@ func (t *FileRoute) getModelFile(c *gin.Context) {
 		c.JSON(http.StatusForbidden, gin.H{"message": "permission denied"})
 		return
 	}
-	fmt.Println("has access")
+
 	var fileType string
 	if fileExt != "glb" {
 		fileType = "images"
@@ -140,7 +138,7 @@ func (t *FileRoute) getModelFile(c *gin.Context) {
 	}
 
 	filePath := fmt.Sprintf(internal.Root+"/uploads/%s/%s/%s.%s", fileType, projectId.String(), modelId.String(), fileExt)
-	fmt.Println("4")
+
 	t.serveFile(c, filePath)
 }
 
