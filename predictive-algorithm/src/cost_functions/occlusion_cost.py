Correct camera rotation

predictive-algorithm/src/cost_functions/occlusion_cost.py
@@ -42,7 +42,7 @@ def cost(state: State):
             valid_coord, ndc_coord = is_in_view(corner, cam_state)
             if not valid_coord:
                 cost += BIG_M + np.linalg.norm(ndc_coord) * 100
-                continue
+                break
 
             point, _ = state.gltf.ray_trace(
                 origin=cam_state.pos, end_point=corner, first_point=True

predictive-algorithm/src/main.py
@@ -1,5 +1,7 @@
 import math
 import os
+
+import pyvistaqt
 from cost_functions import angle_cost
 from algorithms.particle_swarm_opt import optimize_pso
 from algorithms.differential_evolution import optimize_de
@@ -8,10 +10,10 @@ from state import CameraState, State, render_from_state
 import pyvista as pv
 from pyvistaqt import BackgroundPlotter
 import quaternion
-
-# from pygltflib import GLTF2
+from utils import get_seeded_color_rgb
 
 pl = BackgroundPlotter()
+
 face = np.array(
     [
         [5.0, -1.0, -1.0],
@@ -31,16 +33,60 @@ state = State(
         )
     ],
     scale=1,
-    gltf=pv.read("~/Downloads/omnicam/lidar-cpn.glb")
+    gltf=pv.read("~/Downloads/oxygenai models/cpn-lidar.glb")
     .combine()
     .extract_surface()
     .triangulate(),
 )
 
 
+def init_state(pl: pyvistaqt.BackgroundPlotter, state: State):
+    # pl.add_mesh(state.gltf)
+    for i, camera in enumerate(state.cameras):
+        color = get_seeded_color_rgb(i)
+
+        arrow = pv.Arrow(start=(0, 0, 0), direction=(0.0, 0.0, 1.0))
+        camera.meshes.camera_actor = pl.add_mesh(arrow, color=color)
+        silhouette_actor = pl.add_silhouette(
+            arrow,
+            color="white",
+            line_width=8.0,
+        )
+        camera.meshes.camera_silhouette_actor = silhouette_actor
+
+        cone_length = 10.0
+        # Radius = length * tan(half_angle)
+        cone_radius = cone_length * np.tan(np.radians(camera.vfov / 2))
+
+        view_cone = pv.Cone(
+            center=(0, 0, 1 + cone_length / 2),  # Offset center so apex is at origin
+            direction=(0, 0, -1),  # Pointing along Z
+            height=cone_length,
+            radius=cone_radius,
+            resolution=32,
+        )
+
+        # Add the cone with transparency (opacity)
+        camera.meshes.cone_actor = pl.add_mesh(
+            view_cone, color=color, opacity=0.2, style="surface"
+        )
+
+        faces = np.hstack([[4, 0, 1, 2, 3]])
+        face_mesh = pv.PolyData(camera.face, faces=faces)
+        pl.add_mesh(face_mesh, color=color)
+        camera.meshes.face_mesh = face_mesh
+
+    pl.add_axes()
+    pl.show_axes()
+    pl.enable_trackball_style()
+
+
 def main():
+    init_state(pl, state)
     render_from_state(pl, state)
     pl.show()
+
+    breakpoint()
 
     # from cost_functions import total_cost
     # print(total_cost(state))

predictive-algorithm/src/utils.py
@@ -38,14 +38,16 @@ def angle_from_face_normal(
 ) -> Tuple[Quantity[u.radian], Quantity[u.radian]]:
     """Returns vertical and horizontal angles in radian unit"""
 
-    face_center = np.mean(face, axis=0)
-    angle_vec = quaternion.rotate_vectors(angle, [0, 0, 1])
-    look_vec = face_center - pos
+    face_normal = np.cross(face[0] - face[1], face[0] - face[2])
 
-    horizontal_offset = math.atan2(look_vec[1], look_vec[0]) - math.atan2(
+    angle_vec = quaternion.rotate_vectors(angle, [0, 0, 1])
+    # look_vec = face_center - pos
+
+    horizontal_offset = math.atan2(face_normal[1], face_normal[0]) - math.atan2(
         angle_vec[1], angle_vec[0]
     )
 
+    face_center = np.mean(face, axis=0)
     highest_face_center = np.array([face_center[0], face_center[1], face[:, 2].max()])
     look_vec = highest_face_center - pos
     dist_xy_look = math.sqrt(look_vec[0] ** 2 + look_vec[1] ** 2)