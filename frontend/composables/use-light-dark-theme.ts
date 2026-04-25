69a4d0190780b68d22c436b9fc9656681c2463ea
NewBieCoderXD <fruity5555@gmail.com>
2026-04-25 14:51:46 +0700

    Fix: use cookie for theme

--- frontend/composables/use-light-dark-theme.ts
+++ frontend/composables/use-light-dark-theme.ts
@@ -1,36 +1,11 @@
 export type THEME_OPTION = "light" | "dark";
 
 export function useLightDarkTheme() {
-  const theme = ref<"light" | "dark">("light");
-
-  function applyTheme() {
-    if (theme.value === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-    localStorage.setItem("theme", theme.value);
-  }
+  const theme = useCookie<THEME_OPTION | undefined>("theme");
 
   function toggleTheme() {
     theme.value = theme.value === "light" ? "dark" : "light";
-    applyTheme();
   }
-
-  onMounted(() => {
-    const savedTheme = localStorage.getItem("theme");
-    const systemPrefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
-
-    if (savedTheme) {
-      theme.value = savedTheme as "light" | "dark";
-    } else if (systemPrefersDark) {
-      theme.value = "dark";
-    }
-
-    applyTheme();
-  });
 
   return {
     theme,
