486a2a38a160c8b10a79642d5906400e61c6bf7c
Oatkrub <sirawit.vir@builk.com>
2026-05-27 11:28:30 +0700

    feat: alert when disconnect to websocket for autosave and livestream

--- frontend/composables/api/use-autosave-ws.ts
+++ frontend/composables/api/use-autosave-ws.ts
@@ -8,6 +8,11 @@ export function useAutosaveWs(
 ) {
   const runtimeConfig = useRuntimeConfig();
   let autosaveWs: UseWebSocketReturn<unknown> | undefined = undefined;
+  const autosaveAlert = ref<{
+    title: string;
+    message: string;
+    type: "success" | "warning" | "error";
+  } | null>(null);
   if (workspace == "me" && import.meta.client) {
     const base = getApiBaseUrlWithProtocol("websocket", runtimeConfig);
     const websocketUrl = concatUrl(
@@ -15,15 +20,60 @@ export function useAutosaveWs(
       base,
     );
 
+    let wasConnected = false;
+    let isDisconnectedAlertVisible = false;
+
     autosaveWs = useWebSocket(websocketUrl, {
       autoReconnect: {
         delay: 1000,
-        onFailed: () => {
-          alert("Failed to connect websocket after multiple retries.");
-        },
+      },
+
+      onConnected: () => {
+        if (wasConnected) {
+          autosaveAlert.value = {
+            title: "Autosave Reconnected",
+            message: "Autosave connection restored",
+            type: "success",
+          };
+
+          setTimeout(() => {
+            autosaveAlert.value = null;
+          }, 3000);
+        }
+
+        wasConnected = true;
+        isDisconnectedAlertVisible = false;
+      },
+
+      onDisconnected: (ws, event) => {
+        console.log("is this connect : ", wasConnected);
+        if (!wasConnected) return;
+
+        if (isDisconnectedAlertVisible) return;
+
+        isDisconnectedAlertVisible = true;
+
+        const closeEvent = event as CloseEvent;
+
+        if (closeEvent?.code === 1011) {
+          autosaveAlert.value = {
+            title: "Autosave Error",
+            message: closeEvent.reason || "Autosave subscription failed",
+            type: "error",
+          };
+        } else {
+          autosaveAlert.value = {
+            title: "Autosave Disconnected",
+            message: "Changes may not be saved until reconnect",
+            type: "warning",
+          };
+        }
       },
     });
   }
 
-  return { autosaveWs };
+  return {
+    autosaveWs,
+    autosaveAlert,
+  };
 }
