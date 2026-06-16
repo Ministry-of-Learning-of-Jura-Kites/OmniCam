0db591965696074decd83b10dcd6d1df8b182770
Oatkrub <sirawit.vir@builk.com>
2026-06-16 09:16:17 +0700

    fixed: merge conflict

--- backend/internal/routes/api_routes.go
+++ backend/internal/routes/api_routes.go
@@ -106,7 +106,6 @@ func InitRoutes(deps Dependencies, router gin.IRouter) {
 				return r.Header.Get("Origin") == deps.Env.FrontendHost
 			},
 		},
-		Nc: deps.Nc,
 	}
 	cameraAutosaveRoute.InitRoute(protectedRoute)
 
